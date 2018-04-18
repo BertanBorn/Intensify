a858a90ff448f3b650e13eca02d1e503f3b82065
BertanBorn <37880562+BertanBorn@users.noreply.github.com>
2018-04-18 13:24:10 -0400

    Add files via upload
    version 3

--- App.js
+++ App.js
@@ -14,7 +14,7 @@ import {StackNavigator} from 'react-navigation';
 class HeaderPageOne extends React.Component {
   render(){
     return (
-      <Text>What are you doing Today? </Text>
+      <Text style = {Style.headerStyle}>What are you doing Today? </Text>
     );
   }
 }
@@ -22,7 +22,7 @@ class HeaderPageOne extends React.Component {
 class HeaderPageTwo extends React.Component {
   render(){
     return (
-      <Text>Second Page </Text>
+      <Text style = {Style.headerStyle}>What is your Mood? </Text>
     );
   }
 }
@@ -30,7 +30,7 @@ class HeaderPageTwo extends React.Component {
 class HeaderPageThree extends React.Component {
   render(){
     return(
-      <Text>Third Page </Text>
+      <Text style = {Style.headerStyle}>Lets Rock </Text>
     );
   }
 }
@@ -39,10 +39,12 @@ class HeaderPageThree extends React.Component {
 {/*Main content Body*/}
 
 class FirstScreen extends React.Component {
+
+
   static navigationOptions = {
     headerTitle: <HeaderPageOne />,
     headerRight: (
-      <Button style={Style.rightHeaderButton} title="info"
+      <Button style={Style.rightHeaderButton} title="info" color="#190707"
        onPress={()=> alert('This is a button for settings')}/>
     ),
   };
@@ -53,60 +55,68 @@ class FirstScreen extends React.Component {
 
           <View style={Style.optionsMain}>
               <View style={Style.optionsMainSpacing}/>
-              <Button style={Style.optionsMainButton}  title = "Relaxing"
+              <View style={Style.buttonStyle}>
+              <Button style={Style.optionsMainButton} title = "Relaxing" color="#190707"
+              accessibilityLabel = "Page One"
               onPress={() => {
-              this.props.navigation.navigate('pageTwo', { itemId: 1, otherParam: 'Option 1',
+              this.props.navigation.navigate('pageTwo', { page1_Id: 1, otherParam: 'Option 1',
              });
             }}
             />
+              </View>
               <View style={Style.optionsMainSpacing}/>
 
           {/*Button 2 */}
-
-              <Button style={Style.optionsMainButton} title = "Excercising"
+              <View style={Style.buttonStyle}>
+              <Button style={Style.optionsMainButton} title = "Excercising" color="#190707"
               onPress={() => {
-              this.props.navigation.navigate('pageTwo', { itemId: 2, otherParam: 'Option 2',
+              this.props.navigation.navigate('pageTwo', { page1_Id: 2, otherParam: 'Option 2',
               });
               }}
               />
+              </View>
               <View style={Style.optionsMainSpacing}/>
 
           {/*Button 3 */}
-
-              <Button style={Style.optionsMainButton} title = "Studying"
+              <View style={Style.buttonStyle}>
+              <Button style={Style.optionsMainButton} title = "Studying" color="#190707"
                 onPress={() => {
-                this.props.navigation.navigate('pageTwo', { itemId: 3, otherParam: 'Option 3',
+                this.props.navigation.navigate('pageTwo', { page1_Id: 3, otherParam: 'Option 3',
                 });
                 }}
               />
+              </View>
               <View style={Style.optionsMainSpacing}/>
 
           {/*Button 4 */}
-
-              <Button style={Style.optionsMainButton} title = "Partying"
+              <View style={Style.buttonStyle}>
+              <Button style={Style.optionsMainButton} title = "Partying" color="#190707"
                onPress={() => {
-                this.props.navigation.navigate('pageTwo', { itemId: 4, otherParam: 'Option 4',
+                this.props.navigation.navigate('pageTwo', { page1_Id: 4, otherParam: 'Option 4',
                 });
                 }}/>
+              </View>
               <View style={Style.optionsMainSpacing}/>
 
           {/*Button 5 */}
-
-              <Button style={Style.optionsMainButton} title = "Commuting"
+              <View style={Style.buttonStyle}>
+              <Button style={Style.optionsMainButton} title = "Commuting" color="#190707"
               onPress={() => {
-              this.props.navigation.navigate('pageTwo', { itemId: 5, otherParam: 'Option 5',
+              this.props.navigation.navigate('pageTwo', { page1_Id: 5, otherParam: 'Option 5',
               });
               }}/>
+              </View>
               <View style={Style.optionsMainSpacing}/>
 
 
           {/*Button 6 */}
-
-              <Button style={Style.optionsMainButton} title = "Nothing"
+              <View style={Style.buttonStyle}>
+              <Button style={Style.optionsMainButton} title = "Nothing" color="#190707"
               onPress={() => {
-              this.props.navigation.navigate('pageTwo', { itemId: 6, otherParam: 'Option 6',
+              this.props.navigation.navigate('pageTwo', { page1_Id: 6, otherParam: 'Option 6',
               });
               }}/>
+              </View>
               <View style={Style.optionsMainSpacing}/>
           </View>
     );
@@ -115,72 +125,85 @@ class FirstScreen extends React.Component {
 
 {/*###########Page 2############### */}
 class SecondScreen extends React.Component {
+
   static navigationOptions = {
     headerTitle: <HeaderPageTwo />,
     headerRight: (
       <Button style={Style.rightHeaderButton}
-      onPress={()=> alert('This is a button for settings')} title= "info"/>
+      onPress={()=> alert('This is a button for settings')} title= "info" color="#190707"/>
     ),
   };
   render(){
-
+    const {params} = this.props.navigation.state;
+    const otherParam = params ? params.otherParam : null;
+    const page1_Id = params ? params.page1_Id: null;
     return (
 
-          {/*Button 1 */},
 
+          {/*Button 1 */},
           <View style={Style.optionsMain}>
               <View style={Style.optionsMainSpacing}/>
-              <Button style={Style.optionsMainButton}  title = "Happy"
+
+              <View style={Style.buttonStyle}>
+              <Button style={Style.optionsMainButton}  title = "Happy" color="#190707"
               onPress={() => {
-              this.props.navigation.navigate('pageThree', { itemId: 11, otherParam: 'Option 1',
-             });
+              this.props.navigation.navigate('pageThree',
+              {page2_Id: 11, otherParam: 'Option 1', page1_Id});
             }}
             />
+            </View>
               <View style={Style.optionsMainSpacing}/>
 
           {/*Button 2 */}
-
-              <Button style={Style.optionsMainButton} title = "Sad"
+              <View style={Style.buttonStyle}>
+              <Button style={Style.optionsMainButton} title = "Sad" color="#190707"
               onPress={() => {
-              this.props.navigation.navigate('pageThree', { itemId: 22, otherParam: 'Option 2',
-              });
+              this.props.navigation.navigate('pageThree',
+              { page2_Id: 22, otherParam: 'Option 2', page1_Id});
               }}
               />
+              </View>
               <View style={Style.optionsMainSpacing}/>
 
           {/*Button 3 */}
-
-              <Button style={Style.optionsMainButton} title = "Excited"
+              <View style={Style.buttonStyle}>
+              <Button style={Style.optionsMainButton} title = "Excited" color="#190707"
                 onPress={() => {
-                this.props.navigation.navigate('pageThree', { itemId: 33, otherParam: 'Option 3',
-                });
+                this.props.navigation.navigate('pageThree',
+                { page2_Id: 33, otherParam: 'Option 3', page1_Id});
                 }}
               />
+              </View>
               <View style={Style.optionsMainSpacing}/>
 
           {/*Button 4 */}
-
-              <Button style={Style.optionsMainButton} title = "Angry"
+              <View style={Style.buttonStyle}>
+              <Button style={Style.optionsMainButton} title = "Angry" color="#190707"
                onPress={() => {
-                this.props.navigation.navigate('pageThree', { itemId: 44, otherParam: 'Option 4',
-                });
+                this.props.navigation.navigate('pageThree',
+                {page2_Id: 44, otherParam: 'Option 4', page1_Id});
                 }}/>
+              </View>
               <View style={Style.optionsMainSpacing}/>
 
           {/*Button 5 */}
-
-              <Button style={Style.optionsMainButton} title = "Stressed"
+              <View style={Style.buttonStyle}>
+              <Button style={Style.optionsMainButton} title = "Stressed" color="#190707"
               onPress={() => {
-              this.props.navigation.navigate('pageThree', { itemId: 55, otherParam: 'Option 5',
-              });
+              this.props.navigation.navigate('pageThree',
+              {page2_Id: 55, otherParam: 'Option 5', page1_Id });
               }}/>
+              </View>
               <View style={Style.optionsMainSpacing}/>
 
-              <Button style={Style.optionsMainButton} title = "neutral"
+          {/*Button 6 */}
+              <View style={Style.buttonStyle}>
+              <Button style={Style.optionsMainButton} title = "neutral" color="#190707"
               onPress={() => {
-              this.props.navigation.navigate('pageThree', { itemId: 66, otherParam: 'Option 6',
-              });
+              this.props.navigation.navigate('pageThree', {
+              page2_Id: 66, otherParam: 'Option 6', page1_Id });
               }}/>
+              </View>
               <View style={Style.optionsMainSpacing}/>
           </View>
     );
@@ -193,20 +216,20 @@ class SecondScreen extends React.Component {
       headerTitle: <HeaderPageThree />,
       headerRight: (
         <Button style={Style.rightHeaderButton}
-        onPress={()=> alert('This is a button for settings')} title= "info"/>
+        onPress={()=> alert('This is a button for settings')} title= "info" color="#190707"/>
       ),
     };
     render() {
       const {params} = this.props.navigation.state;
-      const itemId = params ? params.itemId:null;
+      const page2_Id = params ? params.page2_Id: null;
       const otherParam = params ? params.otherParam : null;
+      const page1_Id = params ? params.page1_Id: null;
 
       return (
         <View style={Style.optionsMain}>
           <Text> Spotify Music</Text>
-          <Text>itemId: {JSON.stringify(itemId)}</Text>
-          {/*<Text>otherParam: {JSON.stringify(otherParam)}</Text>
-          <Text>Page one input: {JSON.stringify(pageOneInput)} </Text> */}
+          <Text>page2_Id: {JSON.stringify(page2_Id)}</Text>
+          <Text>Page One Id: {JSON.stringify(page1_Id)}</Text>
         </View>
       );
 
